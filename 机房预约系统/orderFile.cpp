85c3e6a66ba3b7e68f247467843a4eeb6c03bebf
liangyulang <1129270420@qq.com>
2022-12-13 17:26:34 +0800

    vsfirstwork

---
+++ 机房预约系统/orderFile.cpp
@@ -0,0 +1,127 @@
+#include"orderFile.h"
+
+//构造函数
+OrderFile::OrderFile()
+{
+	ifstream ifs;
+	ifs.open(ORDER_FILE, ios::in);
+
+	string date;    //日期
+	string interval;//时间段
+	string stuId;   //学生编号
+	string stuName; //学生姓名
+	string roomId;  //机房编号
+	string status;  //预约状态
+
+	this->m_Size = 0; // 记录条数
+
+	while (ifs >> date && ifs >> interval && ifs >> stuId && ifs >> stuName && ifs >> roomId && ifs >> status)
+	{
+		//cout << date << endl;
+		//cout << interval << endl;
+		//cout << stuId << endl;
+		//cout << stuName<< endl;
+		//cout << roomId<< endl;
+		//cout << status<< endl;
+		//cout << endl;
+
+		string key;
+		string value;
+		map<string, string> m;
+
+		int pos = date.find(":");//间隔分开冒号：两边
+		if (pos != -1)
+		{
+			key = date.substr(0,pos);
+			value = date.substr(pos + 1,date.size() - pos - 1);
+
+			m.insert(make_pair(key, value));
+		}
+			//cout << "key=" << key << endl;
+			//cout << "value=" << value << endl;
+		pos = interval.find(":");//间隔分开冒号：两边
+		if (pos != -1)
+		{
+			key = interval.substr(0, pos);
+			value = interval.substr(pos + 1, interval.size() - pos - 1);
+
+			m.insert(make_pair(key, value));
+		}
+
+		pos = stuId.find(":");//间隔分开冒号：两边
+		if (pos != -1)
+		{
+			key = stuId.substr(0, pos);
+			value = stuId.substr(pos + 1, stuId.size() - pos - 1);
+
+			m.insert(make_pair(key, value));
+		}
+
+		pos = stuName.find(":");//间隔分开冒号：两边
+		if (pos != -1)
+		{
+			key = stuName.substr(0, pos);
+			value = stuName.substr(pos + 1, stuName.size() - pos - 1);
+
+			m.insert(make_pair(key, value));
+		}
+
+		pos = roomId.find(":");//间隔分开冒号：两边
+		if (pos != -1)
+		{
+			key = roomId.substr(0, pos);
+			value = roomId.substr(pos + 1, roomId.size() - pos - 1);
+
+			m.insert(make_pair(key, value));
+		}
+
+		pos = status.find(":");//间隔分开冒号：两边
+		if (pos != -1)
+		{
+			key = status.substr(0, pos);
+			value = status.substr(pos + 1, status.size() - pos - 1);
+
+			m.insert(make_pair(key, value));
+		}
+
+		//将小map容器放入到大的map容器中
+		this->m_orderData.insert(make_pair(this->m_Size, m));
+		this->m_Size++;
+	}
+
+	ifs.close();
+
+	//测试最大map容器
+	//for (map<int, map<string, string>>::iterator it = m_orderData.begin(); it != m_orderData.end(); it++)
+	//{
+	//	cout << "条数为： " << it->first << "value = " << endl;
+	//	for (map<string, string>::iterator mit = (*it).second.begin(); mit != it->second.end(); mit++)
+	//	{
+	//		cout << "  key=" << mit->first << "  value=" << mit->second << " ";
+	//	}
+	//	cout << endl;
+	//}
+}
+
+//更新预约记录
+void OrderFile::updateOrder()
+{
+	if (this->m_Size == 0)
+	{
+		return; //预约记录0条，直接return
+	}
+
+	ofstream ofs;
+	ofs.open(ORDER_FILE, ios::out | ios::trunc);
+	for (int i = 0; i < this->m_Size; i++)
+	{
+		ofs << "date:" << this->m_orderData[i]["date"] << " ";
+		ofs << "interval:" << this->m_orderData[i]["interval"] << " ";
+		ofs << "stuId:" << this->m_orderData[i]["stuId"] << " ";
+		ofs << "stuName:" << this->m_orderData[i]["stuName"] << " ";
+		ofs << "roomId:" << this->m_orderData[i]["roomId"] << " ";
+		ofs << "status:" << this->m_orderData[i]["status"] << endl;
+	}
+	ofs.close();
+
+}
---
+++ 机房预约系统/student.h
@@ -0,0 +1,44 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include"Identity.h"
+#include<string>
+#include<vector>
+#include"computerRoom.h"
+#include<fstream>
+#include"globalFile.h"
+#include"orderFile.h"
+
+//学生类
+class Student : public Identity {
+public:
+
+	//默认构造
+	Student();
+
+	//有参构造  参数：学号、姓名、密码
+	Student(int id, string name, string pwd);
+
+	//菜单页面
+	virtual void operMenu() ;
+
+	//申请预约
+	void applyOrder();
+
+	//查看自身预约
+	void showMyOrder();
+
+	//查看所有预约
+	void showAllOrder();
+
+	//取消预约
+	void cancelOrder();
+
+
+	//学生学号
+	int m_Id;
+
+	//机房容器  会调用到机房的信息
+	vector<ComputerRoom> vCom;
+};
+
